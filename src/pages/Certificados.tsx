d83831f203ab3da1dcc4fc04e284efcc3ca93b26
maikol <maikolpltrujillo@gmail.com>
2026-09-01 17:09:17 -0500

    estado de selecionar certificado para una mejor vista

--- src/pages/Certificados.tsx
+++ src/pages/Certificados.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import type { Certificado } from "../types/Certificados"
 import './Cerificados.css'
 
@@ -7,13 +8,17 @@ type CertificadosListProps = {
 }
 
 export function Certificados({ certificado }: CertificadosListProps) {
+
+    const [select,setSelect] = useState<Certificado | null>(null)
     return (
         <div className="certificadosContainer">
             <h2>Certificados De Programación</h2>
             <div className="certificadosGrid">
                 {
                     certificado.map((item) => (
-                        <div key={item.id} className="certificadoCard">
+                        <div key={item.id} className="certificadoCard"
+                         onClick={()=>setSelect(item)}
+                        >
                             <div className="certificadoImgContainer">
                                 <img src={item.img} alt={item.title} />
                             </div>
@@ -22,6 +27,22 @@ export function Certificados({ certificado }: CertificadosListProps) {
                     ))
                 }
             </div>
+            {
+                select && (
+                    <div className="modalOverlay" onClick={()=>setSelect(null)}>
+                        <div className="modalContent" onClick={(e) => e.stopPropagation()}>
+                            <button className="closeBtn" onClick={()=>setSelect(null)}>
+                            &times;
+                            </button>
+
+                            <img src={select.img} alt={select.title} />
+                        <h3>{select.title}</h3>
+
+                        </div>
+
+                    </div>
+                )
+            }
         </div>
     )
 }
